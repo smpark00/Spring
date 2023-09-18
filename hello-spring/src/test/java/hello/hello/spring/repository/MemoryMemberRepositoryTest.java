d99525f8d7a81f514f65d124b0d5fd0114844fed
SeungMin Park <tonydragon@naver.com>
2023-09-19 00:07:49 +0900

    회원 관리 예제

---
+++ hello-spring/src/test/java/hello/hello/spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,12 @@
+package hello.hello.spring.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
+}
