a079457a914337ef2dbd5540561c37680b97da2c
SeungMin Park <tonydragon@naver.com>
2023-09-23 01:48:04 +0900

    회원관리 테스트 케이스

---
+++ hello-spring/src/test/java/hello/hello/spring/service/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
