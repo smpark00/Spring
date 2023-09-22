b8f18650ae9700c40cee711229f09b2f3e5df4f7
SeungMin Park <tonydragon@naver.com>
2023-09-23 00:53:21 +0900

    회원 서비스 개발

--- hello-spring/src/main/java/hello/hello/spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello/spring/service/MemberService.java
@@ -4,11 +4,46 @@ import hello.hello.spring.domain.Member;
 import hello.hello.spring.repository.MemberRepository;
 import hello.hello.spring.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    /**
+     * 회원가입
+     */
     public Long join(Member member){
+        //같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member);
+        /*
+        findByName이 Optional로 정의를 내가 해놨으므로, 이렇게 쓸수 있지만 줄이는게 좋다.
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+         */
 
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m->{
+                throw new IllegalStateException("이미 존재하는 회원입니다");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
