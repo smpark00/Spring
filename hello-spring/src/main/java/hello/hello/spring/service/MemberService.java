ceba1095c645a6034219aef7aa8fa809fe6fd640
SeungMin Park <tonydragon@naver.com>
2023-09-24 16:18:46 +0900

    자동 의존관계 설정

--- hello-spring/src/main/java/hello/hello/spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello/spring/service/MemberService.java
@@ -2,14 +2,18 @@ package hello.hello.spring.service;
 
 import hello.hello.spring.domain.Member;
 import hello.hello.spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- hello-spring/src/test/java/hello/hello/spring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hello/spring/service/MemberServiceTest.java
@@ -51,6 +51,7 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
         /*try {
             memberService.join(member2);
             fail();
