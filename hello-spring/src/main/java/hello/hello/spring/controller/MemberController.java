85c0ebbe53cb7525d355038947308ae59b1b0c83
SeungMin Park <tonydragon@naver.com>
2023-09-29 01:31:04 +0900

    d

--- hello-spring/src/main/java/hello/hello/spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello/spring/controller/MemberController.java
@@ -21,12 +21,12 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm form){
+    public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
 
@@ -36,7 +36,7 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public String list(Model model){
+    public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
